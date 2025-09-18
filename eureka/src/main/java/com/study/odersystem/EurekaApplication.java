9590ef7c89333c1d0e28a71ca4cb8ddd3578aad2
FOJF <john9823@naver.com>
2025-09-18 10:08:13 +0900

    eureka 추가

---
+++ eureka/src/main/java/com/study/odersystem/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.study.odersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer // Eureka 서버로 하려면 꼭 필요한 어노테이션
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
---
+++ eureka/src/test/java/com/study/odersystem/OdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.odersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
