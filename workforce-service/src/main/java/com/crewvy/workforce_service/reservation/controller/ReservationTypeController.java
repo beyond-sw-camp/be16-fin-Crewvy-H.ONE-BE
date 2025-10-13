e9226e80cd8482613efe7152891ae32962c88a17
Jihyeon0804 <cam080421@naver.com>
2025-10-13 12:20:31 +0900

    [수정] Controller 단 메서드 반환값 수정 #33
    수정사항 : ApiResponse -> ResponseEntity
    
    영향 범위 : ReservationTypeController

--- workforce-service/src/main/java/com/crewvy/workforce_service/reservation/controller/ReservationTypeController.java
+++ workforce-service/src/main/java/com/crewvy/workforce_service/reservation/controller/ReservationTypeController.java
@@ -9,6 +9,8 @@ import com.crewvy.workforce_service.reservation.dto.response.ReservationTypeUpda
 import com.crewvy.workforce_service.reservation.service.ReservationTypeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,29 +26,29 @@ public class ReservationTypeController {
     
     // 예약 자원 추가
     @PostMapping("/register")
-    public ApiResponse<?> createResource(@RequestBody ReservationTypeCreateReq reservationTypeCreateReq) {
+    public ResponseEntity<?> createResource(@RequestBody ReservationTypeCreateReq reservationTypeCreateReq) {
         ReservationTypeCreateRes res = reservationTypeService.saveReservationType(reservationTypeCreateReq);
-        return new ApiResponse<>(true, res, "예약 자원 추가 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, res, "예약 자원 추가 성공"), HttpStatus.CREATED);
     }
 
     // 예약 자원 조회
     @GetMapping("/list")
-    public ApiResponse<?> getResourceList(@RequestParam UUID companyId) {
+    public ResponseEntity<?> getResourceList(@RequestParam UUID companyId) {
         List<ReservationTypeListRes> res = reservationTypeService.getResourceList(companyId);
-        return new ApiResponse<>(true, res, "예약 자원 조회 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, res, "예약 자원 조회 성공"), HttpStatus.OK);
     }
 
     // 예약 자원 수정
     @PutMapping("/update/{id}")
-    public ApiResponse<?> updateResource(@PathVariable UUID id, @RequestBody ReservationTypeUpdateReq updateReq) {
+    public ResponseEntity<?> updateResource(@PathVariable UUID id, @RequestBody ReservationTypeUpdateReq updateReq) {
         ReservationTypeUpdateRes res = reservationTypeService.updateReservationType(id, updateReq);
-        return new ApiResponse<>(true, res, "예약 자원 수정 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, res, "예약 자원 수정 성공"), HttpStatus.OK);
     }
 
     // 예약 자원 삭제
     @DeleteMapping("/delete/{id}")
-    public ApiResponse<?> deleteResource(@PathVariable UUID id) {
+    public ResponseEntity<?> deleteResource(@PathVariable UUID id) {
         reservationTypeService.deleteReservationType(id);
-        return new ApiResponse<>(true, null, "예약 자원 삭제 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, null, "예약 자원 삭제 성공"), HttpStatus.OK);
     }
 }
