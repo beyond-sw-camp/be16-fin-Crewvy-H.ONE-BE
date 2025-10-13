8c3db113d6865708b0e0b873dd304056eb971c9f
Jihyeon0804 <cam080421@naver.com>
2025-10-13 12:30:12 +0900

    [수정] Controller 단 메서드 반환값 수정 #37
    수정사항 : ApiResponse -> ResponseEntity
    
    영향 범위 : SalaryInfoController

--- workforce-service/src/main/java/com/crewvy/workforce_service/salary/controller/SalaryInfoController.java
+++ workforce-service/src/main/java/com/crewvy/workforce_service/salary/controller/SalaryInfoController.java
@@ -7,6 +7,8 @@ import com.crewvy.workforce_service.salary.dto.response.SalaryInfoRes;
 import com.crewvy.workforce_service.salary.service.SalaryInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,29 +24,29 @@ public class SalaryInfoController {
 
     // 급여 기본 정보 전체 목록 조회
     @GetMapping("/list")
-    public ApiResponse<List<SalaryInfoRes>> getSalaryInfos(@RequestParam UUID companyId) {
+    public ResponseEntity<?> getSalaryInfos(@RequestParam UUID companyId) {
         List<SalaryInfoRes> response = salaryInfoService.getSalaryInfoList(companyId);
-        return new ApiResponse<>(true, response, "전체 목록 조회 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, response, "전체 목록 조회 성공"), HttpStatus.OK);
     }
 
     // 급여 기본 정보 저장
     @PostMapping("/save")
-    public ApiResponse<List<SalaryInfoRes>> saveSalaryInfo(@RequestBody List<SalaryInfoCreateReq> requests) {
+    public ResponseEntity<?> saveSalaryInfo(@RequestBody List<SalaryInfoCreateReq> requests) {
         List<SalaryInfoRes> response = salaryInfoService.saveSalaryInfoList(requests);
-        return new ApiResponse<>(true, response, "항목 저장 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, response, "항목 저장 성공"), HttpStatus.CREATED);
     }
 
     // 급여 기본 정보 수정
     @PutMapping("/update")
-    public ApiResponse<List<SalaryInfoRes>> updateSalaryInfo(@RequestBody List<SalaryInfoUpdateReq> requests) {
+    public ResponseEntity<?> updateSalaryInfo(@RequestBody List<SalaryInfoUpdateReq> requests) {
         List<SalaryInfoRes> response = salaryInfoService.updateSalaryInfoList(requests);
-        return new ApiResponse<>(true, response, "항목 수정 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, response, "항목 수정 성공"), HttpStatus.OK);
     }
 
     // 급여 기본 정보 삭제
     @DeleteMapping("/delete")
-    public ApiResponse<?> deleteSalaryInfo(@RequestBody List<UUID> uuidList) {
+    public ResponseEntity<?> deleteSalaryInfo(@RequestBody List<UUID> uuidList) {
         salaryInfoService.deleteSalaryInfoList(uuidList);
-        return new ApiResponse<>(true, null, "항목 삭제 성공");
+        return new ResponseEntity<>(new ApiResponse<>(true, null, "항목 삭제 성공"), HttpStatus.OK);
     }
 }
