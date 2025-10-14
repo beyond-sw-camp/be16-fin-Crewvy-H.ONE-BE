4ae9ca161ab9cec52a18775b36018e9ffad2a9dd
kishinoa <chltmdgnl11@gmail.com>
2025-10-14 11:44:16 +0900

    [추가] MemberClient 정의 #52
    원인:
    
    수정사항: member-service와 통신할 MemberClient 정의
    
    영향 범위:

--- workforce-service/src/main/java/com/crewvy/workforce_service/feignClient/MemberClient.java
+++ workforce-service/src/main/java/com/crewvy/workforce_service/feignClient/MemberClient.java
@@ -2,6 +2,7 @@ package com.crewvy.workforce_service.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "member-service")
+@FeignClient(name = "member-client", url = "${feign.client.member-service.url}")
 public interface MemberClient {
+
 }
