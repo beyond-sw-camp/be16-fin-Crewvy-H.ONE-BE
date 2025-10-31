e37c312cd590c5ee811f0bc869c9d6a6011e7061
kishinoa <chltmdgnl11@gmail.com>
2025-10-31 14:48:44 +0900

    [추가] 알림 모두 읽기 API 추가 #112
    원인:
    
    수정사항: 알림 모두 읽기 API 추가
    
    영향 범위:

--- workspace-service/src/main/java/com/crewvy/workspace_service/notification/repository/NotificationRepository.java
+++ workspace-service/src/main/java/com/crewvy/workspace_service/notification/repository/NotificationRepository.java
@@ -2,7 +2,11 @@ package com.crewvy.workspace_service.notification.repository;
 
 import com.crewvy.common.entity.Bool;
 import com.crewvy.workspace_service.notification.entity.Notification;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +15,16 @@ import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     List<Notification> findByReceiverIdAndIsDeleted(UUID memberId, Bool bool);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("UPDATE Notification n " +
+            "SET n.isDeleted = :newStatus " + // <-- (수정) 파라미터로 변경
+            "WHERE n.receiverId = :memberId " +
+            "AND n.isDeleted = :oldStatus") // <-- (수정) 파라미터로 변경
+    int readAllByReceiverId(
+            @Param("memberId") UUID memberId,
+            @Param("newStatus") Bool newStatus, // (추가) 새 상태 파라미터
+            @Param("oldStatus") Bool oldStatus  // (추가) 기존 상태 파라미터
+    );
 }
