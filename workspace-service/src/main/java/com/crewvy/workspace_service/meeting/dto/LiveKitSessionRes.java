0f7502f27eb4764e2ed63bb22e5d3117895fbb48
FOJF <john9823@naver.com>
2025-11-05 12:41:38 +0900

    [수정] 화상회의 목록 조회 응답 수정 #137
    원인 :
    예약된 회의 수정시 이름을 제대로 보여줄 수 없는 문제 발생
    
    수정사항 :
    - 예약된 화상회의 목록 조회 시 초대 유저 목록에 이름도 함께 응답하도록 수정
    - 임포트 문 정리
    영향 범위 :
    - workspace-service

--- workspace-service/src/main/java/com/crewvy/workspace_service/meeting/dto/LiveKitSessionRes.java
+++ workspace-service/src/main/java/com/crewvy/workspace_service/meeting/dto/LiveKitSessionRes.java
@@ -1,12 +1,13 @@
 package com.crewvy.workspace_service.meeting.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class LiveKitSessionRes {
     private UUID videoConferenceId;
     private String token;
