b0c1e9a2dc25e2d562d05e0c53ccb8f2c4dd9ea0
kishinoa <chltmdgnl11@gmail.com>
2025-11-07 12:33:52 +0900

    멤버 필터체인 추가

--- member-service/src/main/java/com/crewvy/member_service/member/config/SecurityConfig.java
+++ member-service/src/main/java/com/crewvy/member_service/member/config/SecurityConfig.java
@@ -3,12 +3,29 @@ package com.crewvy.member_service.member.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/member/login",
+                        "/member/doLogin",
+                        "/member/create-admin",
+                        "/member/check-email"
+                ).permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
